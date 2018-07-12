0b4f9d5d1cdd91909736dc54b34c4fb891b99a10
tkwoo <wtk1101@gmail.com>
2018-07-12 22:52:51 +0900

    [update] face encoding

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ if args.with_draw == 'True':
     cv2.namedWindow('show', 0)
 
 for idx in range(length):
-    bgr_img = vc.read()[1]
-    if bgr_img is None:
+    img_bgr = vc.read()[1]
+    if img_bgr is None:
         break
     if idx%3 != 0: continue
     if idx < 200: continue
@@ -49,11 +49,13 @@ for idx in range(length):
     start = cv2.getTickCount()
     
     ### preprocess
-    bgr_img = preprocess(bgr_img)
+    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    img_bgr_ori = img_bgr.copy()
+    img_bgr = preprocess(img_bgr)
 
     ### detection
-    border = (bgr_img.shape[1] - bgr_img.shape[0])//2
-    bgr_img = cv2.copyMakeBorder(bgr_img,
+    border = (img_bgr.shape[1] - img_bgr.shape[0])//2
+    img_bgr = cv2.copyMakeBorder(img_bgr,
                                  border, # top
                                  border, # bottom
                                  0, # left
@@ -61,9 +63,9 @@ for idx in range(length):
                                  cv2.BORDER_CONSTANT,
                                  value=(0,0,0))
 
-    (h, w) = bgr_img.shape[:2]
+    (h, w) = img_bgr.shape[:2]
     
-    blob = cv2.dnn.blobFromImage(cv2.resize(bgr_img, (300, 300)), 1.0,
+    blob = cv2.dnn.blobFromImage(cv2.resize(img_bgr, (300, 300)), 1.0,
         (300, 300), (104.0, 177.0, 123.0))
     net.setInput(blob)
     detections = net.forward()
@@ -80,15 +82,18 @@ for idx in range(length):
         (l, t, r, b) = box.astype("int") # l t r b
         
         original_vertical_length = b-t
-        t = int(t + (original_vertical_length)*0.15)
-        b = int(b - (original_vertical_length)*0.05)
+        t = int(t + (original_vertical_length)*0.15) - border
+        b = int(b - (original_vertical_length)*0.05) - border
 
         margin = ((b-t) - (r-l))//2
         l = l - margin if (b-t-r+l)%2 == 0 else l - margin - 1
         r = r + margin
-        refined_box = [l, t, r, b]
+        refined_box = [t,r,b,l]
         list_bboxes.append(refined_box)
         list_confidence.append(confidence)
+
+    ### facenet
+    face_encodings = face_recognition.face_encodings(img_rgb, list_bboxes)
 
     time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
     print ('%d, elapsed time: %.3fms'%(idx,time))
@@ -96,18 +101,18 @@ for idx in range(length):
     ### draw rectangle bbox
     if args.with_draw == 'True':
         for bbox, confidence in zip(list_bboxes, list_confidence):
-            l, t, r, b = bbox
+            t,r,b,l = bbox
             
-            cv2.rectangle(bgr_img, (l, t), (r, b),
+            cv2.rectangle(img_bgr_ori, (l, t), (r, b),
                 (0, 255, 0), 2)
             text = "face: %.2f" % confidence
             text_size, base_line = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 2)
             y = t #- 1 if t - 1 > 1 else t + 1
-            cv2.rectangle(bgr_img, (l,y-text_size[1]),(l+text_size[0], y+base_line), (0,255,0), -1)
-            cv2.putText(bgr_img, text, (l, y),
+            cv2.rectangle(img_bgr_ori, (l,y-text_size[1]),(l+text_size[0], y+base_line), (0,255,0), -1)
+            cv2.putText(img_bgr_ori, text, (l, y),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
 
-        cv2.imshow('show', bgr_img)
+        cv2.imshow('show', img_bgr_ori)
         key = cv2.waitKey(1)
         if key == 27:
             break
